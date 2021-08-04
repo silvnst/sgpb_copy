b619b343198843a253bb0d67750ca3bbf706ed6b
silvnst <silvan.stoeckli@intergga,ch>
2021-08-04 10:26:29 +0200

    tinymce weg

--- sgpb/urls.py
+++ sgpb/urls.py
@@ -19,6 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("praxismethoden.urls")),
-    path('tinymce/', include('tinymce.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
 ]
