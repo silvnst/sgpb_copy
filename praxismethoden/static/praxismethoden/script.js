43cd8cc44a696889d1727a7212f943879d6a0a4c
silvnst <silvan.stoeckli@intergga,ch>
2021-12-13 12:13:18 +0100

    next flex update 1

--- praxismethoden/static/praxismethoden/script.js
+++ praxismethoden/static/praxismethoden/script.js
@@ -1,5 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
-        
+    
+    if ( !document.cookie.split(';').some((item) => item.includes('noInfo=true')) ) {
+        document.querySelector('#header-alert').classList.remove('d-none');
+    }
+
     // Highlight active section
     document.querySelectorAll('nav.navbar a').forEach( e => {
         if (e.href === location.href) {
@@ -8,5 +12,15 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     })
 
+    // Header alert
+    document.querySelectorAll('#header-alert').forEach( alertEl => {
+        alertEl.querySelector('button.btn-close').addEventListener('click', () => {
+            alertEl.addEventListener('animationend', () => {
+                alertEl.remove();
+                document.cookie = 'noInfo=true; max-age=60*60; Secure';
+            })
+            alertEl.style.animationPlayState = 'running';
+        })
+    }) 
 })
 
